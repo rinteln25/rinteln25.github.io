185335411aab9686edc4f5ff5b86dfbbb6369eb4
rinteln.photography <johanna.rinteln@gmail.com>
2026-03-23 21:17:32 +0100

    Update script.js

--- script.js
+++ script.js
@@ -7,3 +7,20 @@ document.addEventListener("contextmenu", function(e) {
 document.addEventListener("dragstart", function(e) {
   e.preventDefault();
 });
+
+// um random Bilder für die Alben zu laden
+function setRandomImage(id, images) {
+  const random = images[Math.floor(Math.random() * images.length)];
+  document.getElementById(id).src = random;
+}
+
+setRandomImage("africa-img", [
+  "images/africa25/wilddog4.jpg",
+  "images/africa25/Giraffen.jpg",
+  "images/africa25/Geparden.jpg"
+]);
+
+setRandomImage("europe-img", [
+  "images/albums/europe1.jpg",
+  "images/albums/europe2.jpg"
+]);
